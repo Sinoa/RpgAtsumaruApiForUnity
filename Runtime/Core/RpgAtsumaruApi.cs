c5e8f11a6203031862e7853e3cdd74da552b5412
Sinoa <sinoa.dev@gmail.com>
2018-11-01 02:59:06 +0900

    ストレージAPIをそれなりにいい感じに実装

--- Runtime/Core/RpgAtsumaruApi.cs
+++ Runtime/Core/RpgAtsumaruApi.cs
@@ -13,6 +13,7 @@
 // 2. Altered source versions must be plainly marked as such, and must not be misrepresented as being the original software.
 // 3. This notice may not be removed or altered from any source distribution.
 
+using System;
 using UnityEngine;
 
 namespace RpgAtsumaruApiForUnity
@@ -26,6 +27,16 @@ namespace RpgAtsumaruApiForUnity
         // 定数定義
         private const string CallbackReceiverGameObjectName = "__RPGATSUMARU_CALLBACK_RECEIVER__";
 
+        // クラス変数宣言
+        private static RpgAtsumaruStorage storageApi;
+
+
+
+        /// <summary>
+        /// RpgAtsumaruApiForUnity プラグインが初期化済みかどうか
+        /// </summary>
+        public static bool Initialized => RpgAtsumaruNativeApi.IsInitialized();
+
 
 
         /// <summary>
@@ -35,7 +46,7 @@ namespace RpgAtsumaruApiForUnity
         public static void Initialize()
         {
             // 既に初期化済みなら
-            if (RpgAtsumaruNativeApi.IsInitialized())
+            if (Initialized)
             {
                 // 直ちに終了
                 return;
@@ -45,12 +56,12 @@ namespace RpgAtsumaruApiForUnity
             // コールバックを受け取るためのゲームオブジェクトを生成して、トランスフォームの取得と必要コンポーネントのアタッチをする
             var gameObject = new GameObject(CallbackReceiverGameObjectName);
             var transform = gameObject.GetComponent<Transform>();
-            gameObject.AddComponent<RpgAtsumaruApiCallbackReceiver>();
+            var receiver = gameObject.AddComponent<RpgAtsumaruApiCallbackReceiver>();
 
 
             // ゲームオブジェクトをヒエラルキから姿を消してシーン遷移による削除を受けないようにする
             gameObject.hideFlags = HideFlags.HideInHierarchy;
-            Object.DontDestroyOnLoad(gameObject);
+            UnityEngine.Object.DontDestroyOnLoad(gameObject);
 
 
             // トランスフォームを初期化する
@@ -65,7 +76,7 @@ namespace RpgAtsumaruApiForUnity
                 // オブジェクト名や、コールバック名を設定していく
                 UnityObjectName = CallbackReceiverGameObjectName,
                 GetItemsCallback = nameof(RpgAtsumaruApiCallbackReceiver.OnStorageItemsReceived),
-                SetItemsCallback = nameof(RpgAtsumaruApiCallbackReceiver.OnStorageSetItemCompleted),
+                SetItemsCallback = nameof(RpgAtsumaruApiCallbackReceiver.OnStorageSetItemsCompleted),
                 RemoveItemCallback = nameof(RpgAtsumaruApiCallbackReceiver.OnStorageRemoveItemCompleted),
                 VolumeChangedCallback = nameof(RpgAtsumaruApiCallbackReceiver.OnVolumeChanged),
             };
@@ -74,6 +85,38 @@ namespace RpgAtsumaruApiForUnity
             // 初期化パラメータのJSONデータ化してネイティブAPIの初期化をする
             var jsonData = JsonUtility.ToJson(nativeApiInitializeParam);
             RpgAtsumaruNativeApi.Initialize(jsonData);
+
+
+            // 各APIを処理するクラスのインスタンスを生成
+            storageApi = new RpgAtsumaruStorage(receiver);
+        }
+
+
+        /// <summary>
+        /// RPGアツマールのサーバーストレージを操作するAPIを取得します
+        /// </summary>
+        /// <returns>サーバーストレージ操作APIのインスタンスを返します</returns>
+        /// <exception cref="InvalidOperationException">プラグインが初期化されていません。Initialize関数を呼び出して初期化を完了してください</exception>
+        public static RpgAtsumaruStorage GetStorageApi()
+        {
+            // 例外判定を入れてからAPIのインスタンスを返す
+            ThrowIfNotInitialized();
+            return storageApi;
+        }
+
+
+        /// <summary>
+        /// プラグインが未初期化の場合に例外をスローします
+        /// </summary>
+        /// <exception cref="InvalidOperationException">プラグインが初期化されていません。Initialize関数を呼び出して初期化を完了してください</exception>
+        private static void ThrowIfNotInitialized()
+        {
+            // 未初期化なら
+            if (!Initialized)
+            {
+                // 未初期化例外を吐く
+                throw new InvalidOperationException("プラグインが初期化されていません。Initialize関数を呼び出して初期化を完了してください");
+            }
         }
 
 
@@ -81,22 +124,48 @@ namespace RpgAtsumaruApiForUnity
         /// <summary>
         /// RPGアツマールネイティブAPIからのコールバックを受け付けるレシーバコンポーネントクラスです
         /// </summary>
-        private sealed class RpgAtsumaruApiCallbackReceiver : MonoBehaviour
+        internal sealed class RpgAtsumaruApiCallbackReceiver : MonoBehaviour
         {
+            /// <summary>
+            /// RPGアツマールのサーバーストレージかｒデータを受信したときのイベントです
+            /// </summary>
+            public event Action<string> StorageItemsReceived;
+
+            /// <summary>
+            /// RPGアツマールのサーバーストレージにデータを設定した完了イベントです
+            /// </summary>
+            public event Action StorageSetItemsCompleted;
+
+            /// <summary>
+            /// RPGアツマールのサーバーストレージからデータを削除した完了イベントです
+            /// </summary>
+            public event Action StorageRemoveItemCompleted;
+
+            /// <summary>
+            /// RPGアツマールのマスター音量を変更したときの通知イベントです
+            /// </summary>
+            public event Action<float> VolumeChanged;
+
+
+
             /// <summary>
             /// RPGアツマールのサーバーストレージからすべてのデータをjsonで受け取ったときのイベントを処理します
             /// </summary>
             /// <param name="jsonData">受け取ったデータのjson文字列データ</param>
             public void OnStorageItemsReceived(string jsonData)
             {
+                // イベントにそのまま横流し
+                StorageItemsReceived?.Invoke(jsonData);
             }
 
 
             /// <summary>
             /// RPGアツマールのサーバーストレージにデータを設定した完了イベントを処理します
             /// </summary>
-            public void OnStorageSetItemCompleted()
+            public void OnStorageSetItemsCompleted()
             {
+                // イベントにそのまま横流し
+                StorageSetItemsCompleted?.Invoke();
             }
 
 
@@ -105,6 +174,8 @@ namespace RpgAtsumaruApiForUnity
             /// </summary>
             public void OnStorageRemoveItemCompleted()
             {
+                // イベントにそのまま横流し
+                StorageRemoveItemCompleted?.Invoke();
             }
 
 
@@ -114,6 +185,8 @@ namespace RpgAtsumaruApiForUnity
             /// <param name="volume">変更された音量（OFF 0.0 ～ 1.0 ON）</param>
             public void OnVolumeChanged(float volume)
             {
+                // イベントにそのまま横流し
+                VolumeChanged?.Invoke(volume);
             }
         }
     }
