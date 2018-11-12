0c4c084af54a183dc2be0c7b3d65bf7adac7e4c4
Sinoa <sinoa.dev@gmail.com>
2018-11-13 02:12:35 +0900

    汎用APIのダミークラスを実装

--- Runtime/Api/Dummy/DummyGeneralApi.cs
+++ Runtime/Api/Dummy/DummyGeneralApi.cs
@@ -17,30 +17,70 @@ using System.Threading.Tasks;
 
 namespace RpgAtsumaruApiForUnity
 {
+    /// <summary>
+    /// 汎用APIのダミークラスです。
+    /// 実際の処理は行わず、何を呼び出されたかのログのみ出力します。
+    /// </summary>
     internal sealed class DummyRpgAtsumaruGeneral : RpgAtsumaruGeneral
     {
+        /// <summary>
+        /// DummyRpgAtsumaruGeneral のインスタンスを初期化します
+        /// </summary>
+        /// <param name="receiver">RPGアツマールネイティブAPIコールバックを拾うレシーバ</param>
         internal DummyRpgAtsumaruGeneral(RpgAtsumaruApi.RpgAtsumaruApiCallbackReceiver receiver) : base(receiver)
         {
         }
 
+
+        /// <summary>
+        /// ゲームURLのクエリに設定された値を取得します（RPGアツマールの仕様上クエリの変数名は param1～param9 になります）
+        /// </summary>
+        /// <param name="name">取得したいクエリ名</param>
+        /// <returns>常に空文字列を返します</returns>
         public override string GetQuery(string name)
         {
+            // 空文字列を返す
+            InternalLogger.Log(LogLevel.Log, $"{GetType().Name}.{nameof(GetQuery)}({name})");
             return string.Empty;
         }
 
+
+        /// <summary>
+        /// RPGアツマールのURLを開くポップアップを非同期で表示します
+        /// </summary>
+        /// <param name="url">URLを開くポップアップに表示するURL</param>
+        /// <returns>常に成功を返す完了タスクを返します</returns>
         public override Task<(bool isError, string message)> OpenLinkAsync(string url)
         {
+            // 指定されたURLを開いて成功を返す
+            InternalLogger.Log(LogLevel.Log, $"{GetType().Name}.{nameof(OpenLinkAsync)}({url})");
+            UnityEngine.Application.OpenURL(url);
             return Task.FromResult((false, string.Empty));
         }
 
+
+        /// <summary>
+        /// 指定されたニコニコユーザーIDの作者情報ダイアログを非同期で表示します
+        /// </summary>
+        /// <param name="niconicoUserId">表示するニコニコユーザーID</param>
+        /// <returns>常に成功を返す完了タスクを返します</returns>
         public override Task<(bool isError, string message)> ShowCreatorInformationAsync(int niconicoUserId)
         {
+            // 成功を返す
+            InternalLogger.Log(LogLevel.Log, $"{GetType().Name}.{nameof(ShowCreatorInformationAsync)}({niconicoUserId})");
             return Task.FromResult((false, string.Empty));
         }
 
+
+        /// <summary>
+        /// スクリーンショットを撮った後にTwitter投稿ダイアログを非同期で操作します
+        /// </summary>
+        /// <returns>常に成功を返す完了タスクを返します</returns>
         [System.Obsolete("この関数は、現在RPGツクールMV専用の機能となっています")]
         public override Task<(bool isError, string message)> ScreenshotAsync()
         {
+            // 成功を返す
+            InternalLogger.Log(LogLevel.Log, $"{GetType().Name}.{nameof(ScreenshotAsync)}()");
             return Task.FromResult((false, string.Empty));
         }
     }
